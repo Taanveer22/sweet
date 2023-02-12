a5b25a7b51ae192e7a27363a2548c05214bbcff7
Taanveer22 <125088986+Taanveer22@users.noreply.github.com>
2023-02-13 04:52:33 +0600

    add js

---
+++ proposeScript.js
@@ -0,0 +1,9 @@
+document.getElementById('yes-btn').addEventListener("click", function () {
+    document.getElementById('msg').innerText = "You are a too sweet person and a blessings in my life";
+  
+})
+
+function proposeSender(){
+    document.getElementById('msg').innerText = "Always stay with blessed and make my life more sweeter";
+
+}
